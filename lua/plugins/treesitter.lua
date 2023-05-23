2991b7483d8867cbe817a16ebff59c257178b48c
Tyler Ipson <tyleripson@Tylers-MBP-2.localdomain>
2023-05-22 21:18:15 -0600

    change help to vimdoc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
 		-- Add languages to be installed here that you want installed for treesitter
-		ensure_installed = { 'go', 'lua', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+		ensure_installed = { 'go', 'lua', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
 		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
 		auto_install = false,
