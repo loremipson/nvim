03f2139d61017c2d39a95fb96f153cfea4a78af5
Tyler Ipson <tyler.ipson@gmail.com>
2024-03-15 21:11:39 -0600

    linted

--- lua/plugin-configs/neotest.lua
+++ lua/plugin-configs/neotest.lua
@@ -1,17 +1,19 @@
 local M = {}
 
 function M.setup()
-  local neotest = require("neotest")
+  local neotest = require 'neotest'
   local key = vim.keymap.set
 
-  neotest.setup({
+  neotest.setup {
     adapters = {
-      require("neotest-vitest"),
-    }
-  })
+      require 'neotest-vitest',
+    },
+  }
 
-  key("n", "<leader>t", neotest.run.run)
-  key("n", "<leader>T", function() neotest.run.run(vim.fn.expand('%')) end)
+  key('n', '<leader>t', neotest.run.run)
+  key('n', '<leader>T', function()
+    neotest.run.run(vim.fn.expand '%')
+  end)
 end
 
 return M
