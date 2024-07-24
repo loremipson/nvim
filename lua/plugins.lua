1a5f4e3d23ed9327763b8653cbb679dd84341e27
Tyler Ipson <tyler.ipson@gmail.com>
2024-07-24 08:15:14 -0600

    add neotest dependencies

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -124,8 +124,11 @@ local plugins = {
     'nvim-neotest/neotest',
     event = 'LspAttach',
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
       'marilari88/neotest-vitest',
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
     },
     config = function()
       require('plugin-configs.neotest').setup()
