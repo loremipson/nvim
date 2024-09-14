65870999ba8b428a8d3f7aa2631f9b94d9c2863c
Tyler Ipson <tyler.ipson@gmail.com>
2024-09-14 10:42:03 -0600

    refactor default options

--- lua/options.lua
+++ lua/options.lua
@@ -1,20 +1,43 @@
-local opt = vim.opt
+local options = {
+  -- ui
+  cursorline = true, -- highlight current line
+  relativenumber = true, -- show relative line numbers
+  number = true, -- enable current line number for "hybrid"
+  splitright = true, -- new vertical splits will open to the right of the current one
+  splitbelow = true, -- new splits will be placed below the current one
+  cmdheight = 1, -- more space in the command line for displaying messages
+  showmode = false, -- we don't need to see things like -- INSERT -- anymore
+  signcolumn = 'yes', -- always show the sign column, otherwise it would shift the text each time
+  scrolloff = 8, -- scroll when you are 8 lines away from the top/bottom
+  sidescrolloff = 8, -- same as above, but for columns
 
--- hybrid line numbers
-opt.relativenumber = true
-opt.number = true
+  laststatus = 3, -- global statusline instead of just the current window
 
--- indenting
-opt.expandtab = true
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+  -- indenting
+  expandtab = true,
+  tabstop = 2,
+  softtabstop = 2,
+  shiftwidth = 2,
+  smarttab = true,
+  smartindent = true,
 
-opt.clipboard = 'unnamed,unnamedplus'
+  -- search
+  hlsearch = true, -- highlight search results
+  ignorecase = true, -- case insensitive searching
+  smartcase = true,
 
--- split windows
-opt.splitright = true
-opt.splitbelow = true
+  clipboard = 'unnamed,unnamedplus', -- copy to system clipboard
+  swapfile = false, -- no swap files
+  backup = false, -- no backup files
 
--- no swap files
-opt.swapfile = false
+  completeopt = 'menu,menuone,noselect', -- better completion for menus
+  pumheight = 10, -- max number of entries in the completion menus
+}
+
+vim.opt.shortmess:append 'c' -- don't show redundant completion messages
+vim.opt.shortmess:append 'I' -- don't show 'Insert mode' messages
+vim.opt.whichwrap:append '<,>,[,],h,l' -- allow horizontal/vertical movement
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
