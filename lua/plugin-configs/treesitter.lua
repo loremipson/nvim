3a5637b0f901ad5d15618f8ad5762699cc6c1126
Tyler Ipson <tyler.ipson@gmail.com>
2024-12-26 20:11:03 -0700

    add missing treesitter langs

--- lua/plugin-configs/treesitter.lua
+++ lua/plugin-configs/treesitter.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
   require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'lua', 'vim', 'vimdoc', 'prisma', 'typescript', 'json', 'go', 'astro', 'vue' },
+    ensure_installed = { 'lua', 'vim', 'vimdoc', 'prisma', 'typescript', 'json', 'go', 'gowork', 'gomod', 'gosum', 'sql', 'gotmpl', 'comment', 'astro', 'vue' },
     sync_install = false,
     highlight = {
       enable = true,
