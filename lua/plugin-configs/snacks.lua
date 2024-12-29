e3030c7316227eda25cb9b4d02753160bdab6dee
Tyler Ipson <tyler.ipson@gmail.com>
2024-12-28 18:23:36 -0700

    fix snacks config and support console log output in ts/js scratch buffers

--- lua/plugin-configs/snacks.lua
+++ lua/plugin-configs/snacks.lua
@@ -2,12 +2,71 @@ local M = {}
 function M.setup()
   local snacks = require 'snacks'
   snacks.setup {
-    options = {
-      indent = { enabled = true },
-      lazygit = { enabled = true },
-      scroll = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
+    indent = { enabled = true },
+    lazygit = { enabled = true },
+    scroll = { enabled = true },
+    -- statuscolumn = { enabled = true },
+    words = { enabled = true },
+    scratch = {
+      win_by_ft = {
+        typescript = {
+          keys = {
+            ['source'] = {
+              '<cr>',
+              function(self)
+                local namespace = vim.api.nvim_create_namespace 'node_result'
+                vim.api.nvim_buf_clear_namespace(self.buf, namespace, 0, -1)
+
+                -- Inject script that makes console log output line numbers.
+                local script = [[
+                  'use strict';
+
+                  const path = require('path');
+
+                  ['debug', 'log', 'warn', 'error'].forEach((methodName) => {
+                      const originalLoggingMethod = console[methodName];
+                      console[methodName] = (firstArgument, ...otherArguments) => {
+                          const originalPrepareStackTrace = Error.prepareStackTrace;
+                          Error.prepareStackTrace = (_, stack) => stack;
+                          const callee = new Error().stack[1];
+                          Error.prepareStackTrace = originalPrepareStackTrace;
+                          const relativeFileName = path.relative(process.cwd(), callee.getFileName());
+                          const prefix = `${relativeFileName}:${callee.getLineNumber()}:`;
+                          if (typeof firstArgument === 'string') {
+                              originalLoggingMethod(prefix + ' ' + firstArgument, ...otherArguments);
+                          } else {
+                              originalLoggingMethod(prefix, firstArgument, ...otherArguments);
+                          }
+                      };
+                  });
+                ]]
+                for _, line in pairs(vim.api.nvim_buf_get_lines(self.buf, 0, -1, true)) do
+                  script = script .. line .. '\n'
+                end
+
+                local result = require('plenary.job')
+                  :new({
+                    command = 'node',
+                    args = { '-e', script },
+                  })
+                  :sync()
+
+                if result then
+                  for _, line in pairs(result) do
+                    local line_number, output = line:match '%[eval%]:(%d+): (.*)'
+                    -- Subtract the lines of the injected script.
+                    vim.api.nvim_buf_set_extmark(0, namespace, line_number - 21, 0, {
+                      virt_text = { { output, 'Comment' } },
+                    })
+                  end
+                end
+              end,
+              desc = 'Source buffer',
+              mode = { 'n', 'x' },
+            },
+          },
+        },
+      },
     },
   }
 
@@ -34,6 +93,13 @@ function M.setup()
   vim.keymap.set('n', '<leader>gl', function()
     snacks.lazygit.log()
   end, { desc = 'Lazygit log (cmd)' })
+
+  vim.keymap.set('n', ']]', function()
+    snacks.words.jump(vim.v.count1)
+  end, { desc = 'Next reference' })
+  vim.keymap.set('n', '[[', function()
+    snacks.words.jump(-vim.v.count1)
+  end, { desc = 'Previous reference' })
 
   vim.api.nvim_create_autocmd('User', {
     pattern = 'VeryLazy',
