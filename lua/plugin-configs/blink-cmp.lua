523873c0003b927c56bd4b65332f7f18d042c34e
Tyler Ipson <tyler.ipson@gmail.com>
2025-02-04 15:56:04 -0700

    update plugins to latest

--- lua/plugin-configs/blink-cmp.lua
+++ lua/plugin-configs/blink-cmp.lua
@@ -11,7 +11,7 @@ function M.setup()
     completion = {
       keyword = { range = 'full' },
       accept = { auto_brackets = { enabled = false } },
-      list = { selection = 'auto_insert' },
+      list = { selection = { preselect = false, auto_insert = true } },
       -- Show documentation when selecting a completion item
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
     },
