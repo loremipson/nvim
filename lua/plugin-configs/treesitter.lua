64a9af22d991fd3f0a0f49bea071cac090bc0d67
Tyler Ipson <tyler.ipson@gmail.com>
2026-03-18 21:10:43 -0600

    fix: treesitter needs to use ensure_installed

--- lua/plugin-configs/treesitter.lua
+++ lua/plugin-configs/treesitter.lua
@@ -3,32 +3,32 @@ local M = {}
 function M.setup()
   require('nvim-treesitter').setup {
     sync_install = false,
+    ensure_installed = {
+      'bash',
+      'lua',
+      'vim',
+      'vimdoc',
+      'prisma',
+      'typescript',
+      'json',
+      'regex',
+      'sql',
+      'gotmpl',
+      'comment',
+      'astro',
+      'vue',
+      'yaml',
+      'css',
+      'html',
+      'javascript',
+      'latex',
+      'scss',
+      'typst',
+    },
     highlight = {
       enable = true,
     },
   }
-  require('nvim-treesitter').install({
-    'bash',
-    'lua',
-    'vim',
-    'vimdoc',
-    'prisma',
-    'typescript',
-    'json',
-    'regex',
-    'sql',
-    'gotmpl',
-    'comment',
-    'astro',
-    'vue',
-    'yaml',
-    'css',
-    'html',
-    'javascript',
-    'latex',
-    'scss',
-    'typst',
-  })
 end
 
 return M
