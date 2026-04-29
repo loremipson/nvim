a62c629e7f957c8094ecc73b3423e1be44d866e4
Tyler Ipson <tyler.ipson@gmail.com>
2026-04-29 10:22:50 -0600

    feat: add some qol autocmds

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require 'options'
 require 'keymaps'
+require 'autocommands'
 require 'plugins'
---
+++ lua/autocommands.lua
@@ -0,0 +1,34 @@
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  desc = "Highlight yanked selection",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200, visual = true })
+  end,
+})
+
+-- Open help in vertical split
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  command = "wincmd L",
+})
+
+-- Auto resize splits when resizing nvim window
+vim.api.nvim_create_autocmd("VimResized", {
+  command = "wincmd =",
+})
+
+-- Show cursorline only in active window
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  group = "active_cursorline",
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
