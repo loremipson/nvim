6a893d5b65c388835fe9a9891988e447648e34a8
Tyler Ipson <tyler.ipson@gmail.com>
2026-09-09 20:32:12 -0600

    chore: add some useful default options

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,7 @@
 local options = {
   -- ui
+  termguicolors = true, -- enable 24-bit color
+  updatetime = 200, -- make things that use `CursorHold` feel more responsive
   cursorline = true, -- highlight current line
   relativenumber = true, -- show relative line numbers
   number = true, -- enable current line number for "hybrid"
@@ -10,6 +12,8 @@ local options = {
   signcolumn = 'yes', -- always show the sign column, otherwise it would shift the text each time
   scrolloff = 8, -- scroll when you are 8 lines away from the top/bottom
   sidescrolloff = 8, -- same as above, but for columns
+  winborder = 'rounded', -- rounded borders
+  confirm = true, -- prompt user with unsaved changes instead of erroring
 
   laststatus = 3, -- global statusline instead of just the current window
 
@@ -20,6 +24,9 @@ local options = {
   shiftwidth = 2,
   smarttab = true,
   smartindent = true,
+  autoindent = true,
+  wrap = false,
+  breakindent = true,
 
   -- search
   hlsearch = true, -- highlight search results
@@ -29,6 +36,10 @@ local options = {
   clipboard = 'unnamed,unnamedplus', -- copy to system clipboard
   swapfile = false, -- no swap files
   backup = false, -- no backup files
+  undofile = true, -- persist undo across sessions
+  autoread = true, -- auto reload a file if it changes on disk outside of nvim
+  inccommand = 'split', -- show substitutions as you type before you hit enter
+  timeoutlen = 300, -- make which-key feel snappier
 
   completeopt = 'menu,menuone,noselect', -- better completion for menus
   pumheight = 10, -- max number of entries in the completion menus
