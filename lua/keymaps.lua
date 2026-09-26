5d75d9d73605e9c37b9065ab3a85ba2d995ebbb6
Tyler Ipson <tyler.ipson@gmail.com>
2026-09-26 12:09:24 -0600

    chore: suppress the written message once and for all

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = ' '
 
 vim.keymap.set('i', 'jj', '<Esc>')
+
+-- Suppress pointless written file message entirely
+vim.keymap.set('ca', 'w', function()
+  return (vim.fn.getcmdtype() == ':' and vim.fn.getcmdline() == 'w') and 'silent write' or 'w'
+end, { expr = true })
